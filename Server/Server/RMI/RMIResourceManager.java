8564951371a3c87dc60f8c3976c245a8d7a27645
alex <potteref@gmail.com>
2018-09-24 20:45:06 -0400

    changed constast

--- Server/Server/RMI/RMIResourceManager.java
+++ Server/Server/RMI/RMIResourceManager.java
@@ -20,7 +20,7 @@ public class RMIResourceManager extends ResourceManager
 {
 	private static String s_serverName = "Server";
 	//TODO: REPLACE 'ALEX' WITH YOUR GROUP NUMBER TO COMPILE
-	// private static String s_rmiPrefix = "groupAlex_";
+	private static String s_rmiPrefix = "group01_";
 
 	public static void main(String args[])
 	{
