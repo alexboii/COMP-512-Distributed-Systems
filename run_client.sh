5d2fdf0b70c3b3c89d540007f32b4746637b19c0
alexboii <potteref@gmail.com>
2018-09-27 20:04:46 -0400

    fixed script

--- run_client.sh
+++ run_client.sh
@@ -9,5 +9,10 @@ JAR_LOCATION="$JAR_DIR/$JAR_CLIENT_SUFFIX"
 mvn -f "$CLIENT_DIR" clean compile assembly:single
 cp java.policy "$JAR_DIR"
 
+echo GENERATE POLICY FILE
+echo "grant {" > $JAR_DIR/java.policy
+echo "	permission java.security.AllPermission;" >> $JAR_DIR/java.policy
+echo "};" >> $JAR_DIR/java.policy
+
 
 java -Djava.security.policy=java.policy -Djava.rmi.server.codebase=file:$JAR_LOCATION -jar $JAR_LOCATION
