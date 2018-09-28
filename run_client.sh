bbfb905408baf89a3a880b87367ebf0e3949e281
alexboii <potteref@gmail.com>
2018-09-27 20:06:42 -0400

    fixed script

--- run_client.sh
+++ run_client.sh
@@ -5,6 +5,9 @@ CLIENT_DIR="$ROOT_DIR/Client"
 JAR_DIR="$CLIENT_DIR/target"	
 JAR_CLIENT_SUFFIX="Client-1.0-SNAPSHOT-jar-with-dependencies.jar"
 JAR_LOCATION="$JAR_DIR/$JAR_CLIENT_SUFFIX"
+COMMON_DIR="$ROOT_DIR/Server/Common/pom.xml"
+
+mvn -f $COMMON_DIR clean install
 
 mvn -f "$CLIENT_DIR" clean compile assembly:single
 cp java.policy "$JAR_DIR"
